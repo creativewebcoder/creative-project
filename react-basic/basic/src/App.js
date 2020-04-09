50ac53b0c3693cca45eaafcf4778b9fa837efab2
Md. Razib Hossain <razibdpi@gmail.com>
2020-04-09 16:27:12 +0600

    react basic app added

---
+++ react-basic/basic/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <h1>This is basic react app</h1>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ react-basic/basic/src/layout/navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav className="nav-wrapper grey darken-3">
+            <div className="continer">
+                
+            </div>
+        </nav>
+    )
+}
