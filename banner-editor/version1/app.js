3027f614b6d9b889ddc94a255b37ae02bdcf9e25
Md. Razib Hossain <razibdpi@gmail.com>
2020-04-23 23:48:04 +0600

    banner-editor added

---
+++ banner-editor/version1/app.js
@@ -0,0 +1,5 @@
+
+
+let bannerContainer = document.getElementById('bannerContainer');
+
+console.log(config)
---
+++ banner-editor/version1/config.js
@@ -0,0 +1,10 @@
+var config = {
+    "formate" : "parallax",
+    "adSize": {
+        "width": "1020",
+        "height": "1920"
+    },
+    "images" : {
+        "bgImg": ""
+    }
+}
